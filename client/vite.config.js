de4a857854c78f6bd2f0445bdef3340ff691420c
sandeep1364 <sandeeppolina1364@gmail.com>
2025-06-15 16:44:45 +0530

    vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   server: {
-    proxy: {
+    proxy: command === 'serve' ? {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false,
       }
-    }
+    } : {}
   }
-}) 
+})) 
