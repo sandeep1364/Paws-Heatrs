d68f68b7d5c537b3f4dfacbf5ea8335f0b87906e
sandeep1364 <145897501+sandeep1364@users.noreply.github.com>
2025-06-15 15:05:11 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-
+const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: "http://localhost:5173",
